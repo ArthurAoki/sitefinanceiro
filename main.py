c92e5bc354e1e57b0d93a36c99b34aa08fd518f6
ARTHUR AOKI I TMA ENGENHARIA <arthur.aoki@tmaengenharia.com.br>
2026-03-16 12:56:57 -0300

    Primeiro commit site limpo

--- main.py
+++ main.py
@@ -158,4 +158,4 @@ def download_arquivo(nome_arquivo):
 # RODAR O SERVIDOR
 # ===============================
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3000, debug=True)
+    app.run(host="0.0.0.0", port=10000)
